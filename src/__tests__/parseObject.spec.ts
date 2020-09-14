b1900ce6581e13f932cb54eaf963e7de6f2f8c5f
CuberL <liaoziyue10@gmail.com>
2020-09-14 19:02:41 +0800

    :white_check_mark: Update tests

--- src/__tests__/parseObject.spec.ts
+++ src/__tests__/parseObject.spec.ts
@@ -1,6 +1,7 @@
 import * as object_properties from "./__fixtures__/object.properties.json";
 import * as object_children from "./__fixtures__/object.children.json";
 import * as object_no_properties from "./__fixtures__/object.no.properties.json";
+import * as object_allof from "./__fixtures__/object.allof.json";
 import { parseObject } from "../parseObject";
 
 describe("parseObject", () => {
@@ -65,6 +66,43 @@ describe("parseObject", () => {
       ObjectNode {
         "get": [Function],
         "properties": Object {},
+        "required": Array [],
+        "type": "object",
+      }
+    `);
+  });
+
+  test("Successfully parsed -> allOf", () => {
+    const object_allof_parsed = parseObject(object_allof);
+    expect(object_allof_parsed).toMatchInlineSnapshot(`
+      ObjectNode {
+        "get": [Function],
+        "properties": Object {
+          "field_1": StringNode {
+            "enums": Array [],
+            "format": null,
+            "maxLength": null,
+            "minLength": null,
+            "pattern": null,
+            "type": "string",
+          },
+          "field_2": StringNode {
+            "enums": Array [],
+            "format": null,
+            "maxLength": null,
+            "minLength": null,
+            "pattern": null,
+            "type": "string",
+          },
+          "field_3": NumberNode {
+            "exclusiveMaximum": null,
+            "exclusiveMinimum": null,
+            "maximum": null,
+            "minimum": null,
+            "multipleOf": null,
+            "type": "number",
+          },
+        },
         "required": Array [],
         "type": "object",
       }
