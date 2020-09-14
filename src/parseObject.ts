c303ded2032351d4802a34277cbe4b3e63045c45
CuberL <liaoziyue10@gmail.com>
2020-09-14 19:01:50 +0800

    :bug: Fix bug

--- src/parseObject.ts
+++ src/parseObject.ts
@@ -1,6 +1,7 @@
 import * as _ from 'lodash';
 import { parse, ObjectNode } from './parse'
 import { BaseNode } from '.';
+import { parseAllOf } from './parseAllOf';
 
 function parseObjectProperties(properties: {[K: string]: object}): {[K: string]: BaseNode} {
     if (!properties) {
@@ -31,6 +32,10 @@ function parseObjectChildren(children: {type: string, name: string}[]): {[K: str
 }
 
 export function parseObject(element: object): ObjectNode {
+    if (element['allOf']) {
+        return parseAllOf(element['allOf']) as ObjectNode;
+    }
+
     if (_.keys(_.get(element, 'properties')).length === 0 && _.size(_.get(element, 'children')) === 0) {
         return new ObjectNode({
             properties: {}
