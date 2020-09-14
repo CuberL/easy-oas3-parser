575d09e276e967cc9efd35435ba0945e4ca192cb
CuberL <liaoziyue10@gmail.com>
2020-09-14 17:09:10 +0800

    :white_check_mark: Update tests

--- src/__tests__/parseAllOf.spec.ts
+++ src/__tests__/parseAllOf.spec.ts
@@ -1,5 +1,6 @@
 import { parseAllOf } from "../parseAllOf";
 import { allOf } from "./__fixtures__/allof.json";
+import { allOf as allof_with_oneof } from "./__fixtures__/allof.with.oneof.json";
 
 describe("parseAllOf", () => {
   test("Successfully parsed", () => {
@@ -35,6 +36,95 @@ describe("parseAllOf", () => {
         },
         "required": Array [],
         "type": "object",
+      }
+    `);
+  });
+
+  test("Successfully parsed -> with oneof", () => {
+    const allof_with_oneof_parsed = parseAllOf(allof_with_oneof);
+    expect(allof_with_oneof_parsed).toMatchInlineSnapshot(`
+      OneOfNode {
+        "cases": Array [
+          ObjectNode {
+            "get": [Function],
+            "properties": Object {
+              "field_1": StringNode {
+                "enums": Array [],
+                "format": null,
+                "maxLength": null,
+                "minLength": null,
+                "pattern": null,
+                "type": "string",
+              },
+              "field_2": StringNode {
+                "enums": Array [],
+                "format": null,
+                "maxLength": null,
+                "minLength": null,
+                "pattern": null,
+                "type": "string",
+              },
+              "field_3": NumberNode {
+                "exclusiveMaximum": null,
+                "exclusiveMinimum": null,
+                "maximum": null,
+                "minimum": null,
+                "multipleOf": null,
+                "type": "number",
+              },
+              "field_4": StringNode {
+                "enums": Array [],
+                "format": null,
+                "maxLength": null,
+                "minLength": null,
+                "pattern": null,
+                "type": "string",
+              },
+            },
+            "required": Array [],
+            "type": "object",
+          },
+          ObjectNode {
+            "get": [Function],
+            "properties": Object {
+              "field_1": StringNode {
+                "enums": Array [],
+                "format": null,
+                "maxLength": null,
+                "minLength": null,
+                "pattern": null,
+                "type": "string",
+              },
+              "field_2": StringNode {
+                "enums": Array [],
+                "format": null,
+                "maxLength": null,
+                "minLength": null,
+                "pattern": null,
+                "type": "string",
+              },
+              "field_3": NumberNode {
+                "exclusiveMaximum": null,
+                "exclusiveMinimum": null,
+                "maximum": null,
+                "minimum": null,
+                "multipleOf": null,
+                "type": "number",
+              },
+              "field_5": NumberNode {
+                "exclusiveMaximum": null,
+                "exclusiveMinimum": null,
+                "maximum": null,
+                "minimum": null,
+                "multipleOf": null,
+                "type": "number",
+              },
+            },
+            "required": Array [],
+            "type": "object",
+          },
+        ],
+        "type": "oneOf",
       }
     `);
   });
