f4b97c441c71ad68aff4d9f5c36fd599152249fc
CuberL <liaoziyue10@gmail.com>
2020-09-14 17:08:30 +0800

    :poop: Support oneOf in allOf. But only one is allowed fow now

--- src/parseAllOf.ts
+++ src/parseAllOf.ts
@@ -1,30 +1,44 @@
 import { parseObject } from "./parseObject";
 import * as _ from 'lodash'
-import { ObjectNode } from "./parse";
+import { ObjectNode, OneOfNode, parse } from "./parse";
 
-export function parseAllOf(element: Array<object>): ObjectNode {
-    function getAllOfFields(_allOf: Array<object>): object {
-		if (!_allOf) {
-			return {}
-		}
-		return _.merge(
+export function parseAllOf(elements: Array<object>): ObjectNode | OneOfNode {
+	const allOf = new ObjectNode({
+		properties: _.merge(
 			{},
-			..._allOf.map(sub_elem =>
-				_.merge(
-					{},
-					_.get(sub_elem, 'properties'),
-					getAllOfFields(_.get(sub_elem, 'allOf'))
+			...elements.map(sub_element => {
+				const sub_element_parsed = parse(sub_element);
+				if (sub_element_parsed.isObject()) {
+					return sub_element_parsed.properties
+				}
+			})
+		)
+	})
+
+	// FIXME: Only support one oneOf for now
+	const one_ofs = elements.filter(element => element['oneOf'])
+	if (one_ofs.length > 0) {
+		const one_of_parsed = parse(one_ofs[0]) as OneOfNode
+		return new OneOfNode(
+			{
+				cases: one_of_parsed.cases.map(
+					_case => {
+						if (_case.isObject()) {
+							return new ObjectNode(
+								{
+									properties: {
+										..._case.properties,
+										...allOf.properties
+									}
+								}
+							)
+						}
+						return allOf
+					}
 				)
-			)
-		);
-	};
-	
-	const allOf = parseObject(
-		{
-			type: 'object',
-			properties: getAllOfFields(element)
-		}
-	)
-	// console.log(allOf)
+			}
+		)
+	}
+
 	return allOf
 }
